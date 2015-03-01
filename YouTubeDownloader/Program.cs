41de616f877e356c0b2ed54a5b3a9809818b665c
Manoj <mkmpvtltd@gmail.com>
2015-03-01 14:23:11 +0545

    first init

---
+++ YouTubeDownloader/Program.cs
@@ -0,0 +1,27 @@
+﻿//Copy rights are reserved for Akram kamal qassas
+//Email me, Akramnet4u@hotmail.com
+using System;
+using System.Net;
+using System.Web;
+using System.Windows.Forms;
+
+namespace YouTube_Downloader
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            System.Net.HttpWebRequest.DefaultWebProxy = Helper.InitialProxy();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new mainForm());
+        }
+
+
+
+    }
+}
